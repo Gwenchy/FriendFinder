bfa7d8ef41435ee5a52b371e59d6aab3b6bccf62
Edgardo Mendez-Soto <gwenchy15@gmail.com>
2018-02-02 11:44:35 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var tableArray = [
+    {
+      name: "Eddie",
+      photo: "eddie@example.com",
+      scores: [
+        1, 
+        2, 
+        3, 
+        4, 
+        5
+      ],
+      
+      name: "Edd",
+      photo: "edd@example.com",
+      scores: [
+        5, 
+        4, 
+        3, 
+        2, 
+        1
+      ]
+    }
+  ];
+  
+  module.exports = tableArray;
+  
